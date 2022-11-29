9378355998247218860e55d4f02e78cc75ca1757
1335697448 <119424483+1335697448@users.noreply.github.com>
2022-11-29 21:39:48 +0800

    1

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -4,18 +4,35 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FT is ERC20 {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+    address Creator;
+    bool TransferState;
+    constructor(string memory name, string memory symbol) ERC20(name,symbol) {
+        Creator = msg.sender;
     }
 
     // TODO 实现mint的权限控制，只有owner可以mint
     function mint(address account, uint256 amount) external {
-
+        require(msg.sender == Creator);
+        _mint(account,amount);
     }
 
     // TODO 用户只能燃烧自己的token
     function burn(uint256 amount) external {
-
+        _burn(msg.sender,amount);
     }
-
+    
     // TODO 加分项：实现transfer可以暂停的逻辑
+    function transfer(address sender,address recipient,uint256 amount) external returns(bool){
+        require(TransferState == true);
+        _transfer(sender, recipient, amount);
+    }
+    function transferState(bool _state) external returns(bool){
+        require(msg.sender == Creator);
+        if(_state == true){
+        TransferState = true;
+        }else{
+            TransferState = false;
+        }
+    }
 }
+
