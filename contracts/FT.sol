af1847de16194f921a7d1fff945cc016bcd0bd72
chengyuanxiang1 <115382335+chengyuanxiang1@users.noreply.github.com>
2022-11-29 23:50:03 +0800

    Update FT.sol

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -9,13 +9,22 @@ contract FT is ERC20 {
 
     // TODO 实现mint的权限控制，只有owner可以mint
     function mint(address account, uint256 amount) external {
-
+    require(account==msg.sender,"only onwer can mint");
+       require(account!=address(0));
+       require(amount>0);
+       super._mint(account,amount);
     }
 
     // TODO 用户只能燃烧自己的token
     function burn(uint256 amount) external {
-
+    require(amount>0);
+    super._mint(msg.sender,amount);
     }
 
     // TODO 加分项：实现transfer可以暂停的逻辑
+    function transfer(address to, uint tokens) public returns (bool success); 
+    balances[msg.sender] = balances[msg.sender].sub(tokens);
+    balances[to] = balances[to].add(tokens);
+    Transfer(msg.sender, to, tokens);
+    return true;
 }
