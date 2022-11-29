3a854ce59f5d9a4ebb0fb81e3a37b0004a0e8e0a
anweicaiwei <111988961+anweicaiwei@users.noreply.github.com>
2022-11-29 21:05:04 +0800

    Update NFT.sol

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -4,26 +4,52 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NFT is ERC721 {
+    uint256[] total;
+    
+    mapping(address => mapping(uint256 => uint256)) TokenOfOwnerByIndex;
+    mapping(uint256 => uint256) OwnerByIndex;    
+    mapping(uint256 => uint256) TokenByIndex;
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {
     }
 
     function mint(address account, uint256 tokenId) external {
-        // TODO
+ 
+        require(tokenId>=0,"not in scope");
+        _mint(account,tokenId);
+        uint256 length = balanceOf(account)-1;
+        TokenOfOwnerByIndex[account][length] = tokenId;
+        OwnerByIndex[tokenId] = length;
+        TokenByIndex[tokenId] = total.length;
+        total.push(tokenId);
+
     }
 
     function burn(uint256 tokenId) external {
-        // TODO 用户只能燃烧自己的NFT
+        require(msg.sender == ownerOf(tokenId),"not owner");
+        _burn(tokenId);
+        uint balance = balanceOf(msg.sender);
+        uint index = OwnerByIndex[tokenId];
+        TokenOfOwnerByIndex[msg.sender][index] = TokenOfOwnerByIndex[msg.sender][balance];
+        OwnerByIndex[TokenOfOwnerByIndex[msg.sender][index]] = index;
+        delete TokenOfOwnerByIndex[msg.sender][balance];
+        
+        uint aIndex = TokenByIndex[tokenId];
+        total[aIndex] = total[total.length-1];
+        total.pop();
+        TokenByIndex[total[aIndex]] = aIndex;
     }
 
     function totalSupply() external view returns (uint256) {
-        // TODO 获取总mint的NFT的数量
+        return total.length;
     }
 
     function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256) {
-        // TODO 加分项：根据用户的index，获取tokenId
+        require(index<balanceOf(owner),"out of range");
+        return TokenOfOwnerByIndex[owner][index];
     }
 
     function tokenByIndex(uint256 index) external view returns (uint256) {
-        // TODO 根据index获取全局的tokenId
+        require(index<total.length,"out of range");
+        return TokenByIndex[index];
     }
 }
