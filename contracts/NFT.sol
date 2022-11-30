4ed47a1256a9b97d5ea686b4d79c6d1f1f4e9032
wsw <1551232467@qq.com>
2022-11-30 11:49:11 +0800

    testWSW

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -5,15 +5,17 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NFT is ERC721 {
     // Token名称
-    string public override _name;
+    string public _name;
     // Token代号
-    string public override _symbol;
-    //  
+    string public _symbol;
+    // NFT 数组
     uint256[] public NFTS; 
-    //
+    // tokenId 到 NFT数组的 index
     mapping(uint256 => uint256) tokenIdToNFTSIndex;
-    //  address 到 mapping index 到 tokenId;
+    // address 到 mapping index 到 tokenId;
     mapping(address => mapping(uint256 => uint256)) private ownerTokens;
+    // tokenId 到 ownnerTokens[address]的 index;
+    mapping(uint256 => uint256) private tokenIdToOwnerIndex;
     // tokenId 到 owner address 的持有人映射
     mapping(uint => address) private _owners;
     // address 到 持仓数量 的持仓量映射
@@ -28,12 +30,13 @@ contract NFT is ERC721 {
         super._mint(account,tokenId);
 
         _balances[account] ++;
-        _owners[account] = tokenId;
+        _owners[tokenId] = account;
 
         NFTS.push(tokenId);
         tokenIdToNFTSIndex[tokenId] = NFTS.length-1;
         uint256 balance = _balances[account];
         ownerTokens[account][balance] = tokenId;
+        tokenIdToOwnerIndex[tokenId] = balance;
 
         emit Transfer(address(0), account, tokenId);
     }
@@ -42,16 +45,15 @@ contract NFT is ERC721 {
         // TODO 用户只能燃烧自己的NFT
         super._burn(tokenId);
 
-        _balances[owner] -= 1;
-        delete _owners[owner];
+        _balances[msg.sender] -= 1;
+        delete _owners[tokenId];
         
         removeList(tokenId);
-        emit Transfer(owner, address(0), tokenId);
     }
 
     function totalSupply() external view returns (uint256) {
         // TODO 获取总mint的NFT的数量
-        returns NFTS.length;
+        return NFTS.length;
     }
 
     function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256) {
@@ -61,14 +63,15 @@ contract NFT is ERC721 {
 
     function tokenByIndex(uint256 index) external view returns (uint256) {
         // TODO 根据index获取全局的tokenId
-        return NFTs[index];
+        return NFTS[index];
     }
 
     function removeList(uint256 tokenId) internal{
         uint256 NFTSIndex = tokenIdToNFTSIndex[tokenId];
-        uint256 NFTS[NFTSIndex] = NFTS[NFTS.length-1];
+        NFTS[NFTSIndex] = NFTS[NFTS.length-1];
         NFTS.pop();
-        tokenIdToNFTSIndex[tokenId].pop();
-        
+        delete tokenIdToNFTSIndex[tokenId]; 
+        uint256 OwnerIndex = tokenIdToOwnerIndex[tokenId];
+        delete ownerTokens[msg.sender][OwnerIndex];   
     }
 }
