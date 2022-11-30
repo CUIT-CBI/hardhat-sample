d29f026c6e5de6daa7c90ba7f1e6854480b7045b
Cofeesy <2898939720@qq.com>
2022-11-30 23:19:49 +0800

    Update contracts\FT.sol Update contracts\NFT.sol

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -4,8 +4,11 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FT is ERC20 {
+
+
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
     }
+
 
     // TODO 实现mint的权限控制，只有owner可以mint
     function mint(address account, uint256 amount) external {
@@ -18,4 +21,17 @@ contract FT is ERC20 {
     }
 
     // TODO 加分项：实现transfer可以暂停的逻辑
+    //hook
+    //Hooks are simply functions that are called before or after some action takes place
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal virtual override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+
+        require(_validRecipient(to), "ERC20WithSafeTransfer: invalid recipient");
+    }
+   
+    function _validRecipient(address to) private view returns (bool) {
+        return (to != address(0));
+    }
 }
