5c62bafea8a151fd91d15ed08dab640b1a84dcfa
penrose <119467288+Penrosc@users.noreply.github.com>
2022-11-30 10:34:18 +0800

    update

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -2,11 +2,15 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./Lock.sol";
 
 contract FT is ERC20 {
-    bool public paused = false;
+
+    uint public unlockTime;
+    address payable public owner;
 
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        owner = payable(msg.sender);
         _mint(msg.sender, 1000000000000000000000000);
     }
     
@@ -25,9 +29,14 @@ contract FT is ERC20 {
     }
 
     // TODO 加分项：实现transfer可以暂停的逻辑
+    function pausedTransfer(uint256 _unlockTime) onlyOwner external {
+        unlockTime = _unlockTime;
+    }
+
     function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
-        require(!paused, "transfer is paused");
-        _transfer(_msgSender(), recipient, amount);
-        return true;
+       require(block.timestamp >= unlockTime,"the transfer still paused");
+       require(msg.sender == owner,"you are not the owner");
+       _transfer(msg.sender, recipient, amount);
+       return true;
     }
 }
