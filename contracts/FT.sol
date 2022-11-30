6d240ef9199983311523388d52fe5ca0fed413ea
litangdingz <106912681+litangdingz@users.noreply.github.com>
2022-11-30 21:39:47 +0800

    Update FT.sol

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -9,12 +9,17 @@ contract FT is ERC20 {
 
     // TODO 实现mint的权限控制，只有owner可以mint
     function mint(address account, uint256 amount) external {
+        require(account != address(0), "ERC20: mint to the zero address");
+        require(account == msg.sender,"only owner can mint");
+        require(amount > 0,"amount");
+        super._mint(account,amount);
 
     }
 
     // TODO 用户只能燃烧自己的token
     function burn(uint256 amount) external {
-
+        require(amount > 0);
+        super._mint(msg.sender,amount);
     }
 
     // TODO 加分项：实现transfer可以暂停的逻辑
