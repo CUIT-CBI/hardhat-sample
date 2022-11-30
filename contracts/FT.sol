4c4fc225c928816f01b19c6aaf814a23ffd4d5f4
yuxuanzhang123456 <106661029+yuxuanzhang123456@users.noreply.github.com>
2022-11-30 21:40:45 +0800

    Update FT.sol

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -1,33 +1,43 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
 
-import "./ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 
-contract ZYX is ERC721 {
-    uint public MAX_LPLS = 10; // 总量
-    address owner;
-    uint tokenId;
+contract FT is ERC20, Pausable {
 
-    // 构造函数
-    constructor(string memory name_, string memory symbol_)
-        ERC721(name_, symbol_)
-    {
-        owner = msg.sender;
-    }
+        uint public unlockTime;
+        address public owner;
 
-    modifier onlyOwner {
-        require(msg.sender == owner,"not owner");
-        _;
-    }
+        constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+            owner == msg.sender;
+        }
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "https://voidtech.cn/i/2022/11/21/xsj7xh.jpg";
-    }
+        modifier onlyOwner() {
+            require(msg.sender == owner);
+            _;
+        }
 
-    // 铸造函数
-    function mint(address to) external onlyOwner {
-        tokenId = tokenId + 1;
-        require(tokenId >= 0 && tokenId < MAX_LPLS, "tokenId out of range");
-        _mint(to, tokenId);
-    }
+       // TODO 实现mint的权限控制，只有owner可以mint
+       function mint(address account, uint256 amount) external onlyOwner{
+           _mint(account, amount);
+       }
+
+       // TODO 用户只能燃烧自己的token
+       function burn(uint256 amount) external {
+           _burn(msg.sender, amount);
+       }
+
+       // TODO 加分项：实现transfer可以暂停的逻辑
+       function myPaused() public onlyOwner{
+           _pause();
+       }
+       function myUnPaused() public onlyOwner{
+           _unpause();
+       }
+
+       function _beforeTokenTransfer(address from, address to, uint256 amount) internal override {
+          require(!paused(),"token transfer while paused");
+       }
+
 }
