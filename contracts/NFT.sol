fc08c4a6172846d34b0ad71349c7804835e2afe7
confucian <confucian.e@gmail.com>
2022-11-30 11:51:10 +0800

    finish NFT basic

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -2,21 +2,32 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NFT is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _counter;
+
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {
     }
 
     function mint(address account, uint256 tokenId) external {
         // TODO
+        _safeMint(account, tokenId, "");
+        _counter.increment();
     }
 
     function burn(uint256 tokenId) external {
         // TODO 用户只能燃烧自己的NFT
+        address owner = ownerOf(tokenId);
+        require(msg.sender == owner, "Only support burn your NFT");
+        _burn(tokenId);
+        _counter.decrement();
     }
 
     function totalSupply() external view returns (uint256) {
         // TODO 获取总mint的NFT的数量
+        return _counter.current();
     }
 
     function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256) {
