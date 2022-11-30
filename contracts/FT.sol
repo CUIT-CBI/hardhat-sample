353e54f57ff079f70b44b55d08e58262a16a9da6
yan <105793954+bengda233@users.noreply.github.com>
2022-11-30 20:08:27 +0800

    Update FT.sol

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -2,20 +2,42 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract FT is ERC20 {
+import "@openzeppelin/contracts/security/Pausable.sol";
+contract FT is ERC20 ,Pausable 
+{
+    uint public unlockTime;
+    address public owner;
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+      owner=msg.sender;
+    }
+    modifier onlyOwner(){
+        require(msg.sender==owner);
+        _;
     }
 
     // TODO 实现mint的权限控制，只有owner可以mint
-    function mint(address account, uint256 amount) external {
-
+    function mint(address account, uint256 amount) external onlyOwner{
+     _mint(account,amount);
     }
 
     // TODO 用户只能燃烧自己的token
     function burn(uint256 amount) external {
-
+      _burn(msg.sender,amount);
     }
 
     // TODO 加分项：实现transfer可以暂停的逻辑
+      function Paused()public onlyOwner{
+        _pause();
+      }
+      function UnPaused()public onlyOwner{
+        _unpause();
+      }
+
+        function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+        ) internal override {
+          require(!paused(),"token transfer while paused");
+    }
 }
