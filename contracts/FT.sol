a1a065ce132922b3bf97df5872eb9aec20a0681f
victorianDan <118161080+victorianDan@users.noreply.github.com>
2022-12-01 20:58:52 +0800

    11

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -2,20 +2,41 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 
-contract FT is ERC20 {
+contract FT is ERC20,Pausable {
+    address public owner;
+
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner);
+        _;
     }
 
     // TODO 实现mint的权限控制，只有owner可以mint
-    function mint(address account, uint256 amount) external {
-
+    function mint(address account, uint256 amount) external onlyOwner{
+        //只有owner可以mint
+        _mint(account, amount);
     }
 
     // TODO 用户只能燃烧自己的token
     function burn(uint256 amount) external {
-
+        //判断amount
+        require(balanceOf(msg.sender) >= amount);
+        _burn(msg.sender, amount);
     }
 
     // TODO 加分项：实现transfer可以暂停的逻辑
+    
+    //在转账之前检查是否被暂停
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override {
+        _requireNotPaused();
+    }
+    //在转账之前owner调用方法暂停交易
+    function pause() external onlyOwner {
+        _pause();
+    }
 }
--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -4,26 +4,87 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NFT is ERC721 {
+    uint private tokenNumber;
+
+    mapping(uint256 => uint256) tokenIdOfIndex;//tokeid => index
+    mapping(address => mapping(uint => uint)) tokeIdByIndex; // address => index => tokeid总数
+
+    //全局tokens数组
+    uint[] private tokens;
+    mapping (uint256 => uint256) globalTokeOfIndex; //tokenId => index
+
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {
     }
 
     function mint(address account, uint256 tokenId) external {
-        // TODO
+        require(msg.sender == account);
+
+        uint indexAll = balanceOf(account);
+        tokeIdByIndex[account][indexAll] = tokenId;
+        tokenIdOfIndex[tokenId] = indexAll;
+
+        //修改合约代码，使得只有owner才可以mint
+        _mint(account,tokenId);
+        //添加token到数组
+        tokens.push(tokenId);
+        //维护mapping
+        globalTokeOfIndex[tokenId] = tokens.length - 1;
+
+        tokenNumber++;
     }
 
     function burn(uint256 tokenId) external {
-        // TODO 用户只能燃烧自己的NFT
+        // 用户只能燃烧自己的NFT
+        require(ownerOf(tokenId) == msg.sender);
+        //获取NFT的主人
+        address owner = ownerOf(tokenId);
+        //获取最后一个NFT的下标
+        uint256 lastIndex = balanceOf(owner) - 1;
+        //最后一个的tokenID
+        uint256 tokeIdLast = tokeIdByIndex[owner][lastIndex];
+        //获取tokenId的对应的index
+        uint256 index = tokenIdOfIndex[tokenId];
+        //将要burn的NFT替换为最后一个tokenId
+        tokeIdByIndex[owner][index] = tokeIdLast;
+        //删除burn的NFT
+        delete tokeIdByIndex[owner][lastIndex];
+        delete tokenIdOfIndex[tokenId];
+        //维护tokenIdOfIndex 
+        tokenIdOfIndex[tokeIdLast] = index;
+
+        //获取数组最后一个token的下标
+        uint256 globalLastIndex = tokens.length - 1;
+        //获取数组最后一个tokenId
+        uint256 globalTokenIdLast = tokens[globalLastIndex];
+        //得到burn的token的下标
+        uint256 globalIndex = globalTokeOfIndex[tokenId];
+        //将burn的token替换为最后一个
+        tokens[globalIndex] = globalTokenIdLast;
+        //将数组最后一个token移除
+        tokens.pop();
+        //维护mapping
+        globalTokeOfIndex[globalTokenIdLast] = globalIndex;
+        delete globalTokeOfIndex[tokenId];
+
+        _burn(tokenId);
+        tokenNumber--;
     }
 
     function totalSupply() external view returns (uint256) {
-        // TODO 获取总mint的NFT的数量
+        // 获取总mint的NFT的数量
+        return tokenNumber;
     }
 
     function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256) {
-        // TODO 加分项：根据用户的index，获取tokenId
+        // 加分项：根据用户的index，获取tokenId
+        //获取NFT的总量
+        require(index < balanceOf(owner));
+        return tokeIdByIndex[owner][index];
     }
 
     function tokenByIndex(uint256 index) external view returns (uint256) {
         // TODO 根据index获取全局的tokenId
+        require(index < tokens.length);
+        return tokens[index];
     }
 }
