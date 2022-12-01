8f0812cac2b8206e9e10d245c0230cc30ec56125
leoaw <3353284242@qq.com>
2022-12-01 14:45:13 +0800

    补充提交

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -6,26 +6,39 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract NFT is ERC721 {
 
     uint256 counter = 0;
-    uint256 ind = 1;
-    mapping(address => mapping(uint256 => uint256)) private _user;
-    mapping(uint256 => uint256) private _list;
+    uint256 ind = 0;
+    mapping(address => uint256[]) private _user;
+     //mapping(uint256 => uint256) private _list;
+    uint256[] private _list;
+    mapping(uint256 => uint256) private _reverList;
 
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {
     }
     
     function mint(address account, uint256 tokenId) external {
         // TODO
-       _mint(account, tokenId);
-       counter++;  
-       _user[account][ind] = tokenId;
-       _list[ind] = tokenId;
-       ind++;
+        _mint(account, tokenId);
+        counter++;  
+        _user[account].push(tokenId);
+        _list.push(tokenId);
+        _reverList[tokenId] = ind;
+        ind++;
     }
 
     function burn(uint256 tokenId) external {
         // TODO 用户只能燃烧自己的NFT
         require(_ownerOf(tokenId) == msg.sender, "NOT your token!");
         _burn(tokenId);
+        uint256 temp = _reverList[tokenId];
+        if(temp == _list.length-1){
+            _list.pop();
+            _user[msg.sender].pop();
+        }else{
+            _list[temp] = _list[_list.length-1];
+            _user[msg.sender][temp] = _user[msg.sender][_user[msg.sender].length-1];
+            _list.pop();
+            _user[msg.sender].pop();
+        }
         counter--;
     }
 
@@ -37,13 +50,13 @@ contract NFT is ERC721 {
     function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256) {
         // TODO 加分项：根据用户的index，获取tokenId
         require(owner != address(0), "NULL ADDRESS!");
-        require(_user[owner][index] != 0, " index OUTOFRANGE!");
+        require(index <= _list.length - 1, " index OUTOFRANGE!");
         return _user[owner][index];
     }
 
     function tokenByIndex(uint256 index) external view returns (uint256) {
         // TODO 根据index获取全局的tokenId
-        require(_list[index] != 0, " index OUTOFRANGE!");
+        require(index <= _list.length - 1, " index OUTOFRANGE!");
         return _list[index];
     }
 }
