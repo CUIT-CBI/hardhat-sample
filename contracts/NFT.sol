c24f2c1dfad68107eb5da056505d86a839c98951
yijia-liu <1763474502@qq.com>
2022-12-01 14:59:05 +0800

    第二次实验

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -4,26 +4,53 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NFT is ERC721 {
+
+    uint256 internal totalSupplies;
+    mapping(uint256 => uint256) public indexToTokenId;
+    mapping(address => uint256[]) public ownerIndexToTokenId;
+    mapping(uint256 => uint256) public tokenIdToIndex;
+
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {
+        totalSupplies = 0;
     }
 
     function mint(address account, uint256 tokenId) external {
         // TODO
+        super._mint(account, tokenId);
+        // token总供应量增加
+        totalSupplies++;
+        ownerIndexToTokenId[account].push(tokenId);
+        tokenIdToIndex[tokenId] = ownerIndexToTokenId[account].length - 1;
+        indexToTokenId[totalSupplies] = tokenId;
     }
 
     function burn(uint256 tokenId) external {
         // TODO 用户只能燃烧自己的NFT
+        super._burn(tokenId);
+        uint256[] storage tokens = ownerIndexToTokenId[msg.sender];
+        tokenIdToIndex[tokens[tokens.length - 1]] = tokenIdToIndex[tokenId];
+        uint256 temp = tokens[tokenIdToIndex[tokenId]];
+        tokens[tokenIdToIndex[tokenId]] = tokens[tokens.length - 1];
+        tokens[tokens.length - 1] = temp;
+        tokens.pop();
+        delete tokenIdToIndex[tokenId];
+        totalSupplies--;
     }
 
     function totalSupply() external view returns (uint256) {
         // TODO 获取总mint的NFT的数量
+        return totalSupplies;
     }
 
     function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256) {
         // TODO 加分项：根据用户的index，获取tokenId
+        uint256[] memory tokens = ownerIndexToTokenId[owner];
+        require(tokens.length > index, "index not have token");
+        return tokens[index];
     }
 
     function tokenByIndex(uint256 index) external view returns (uint256) {
         // TODO 根据index获取全局的tokenId
+        return indexToTokenId[index];
     }
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,7 +10,7 @@ const config: HardhatUserConfig = {
     },
     localhost: {
       url: "http://127.0.0.1:8545/",
-      accounts: ["0xdf57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e", "0xde9be858da4a475276426320d5e9262ecfc3ba460bfac56360bfa6c4c28b4ee0"]
+      accounts: ["0xde9be858da4a475276426320d5e9262ecfc3ba460bfac56360bfa6c4c28b4ee0", "0xdf57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e"]
     },
     heco_testnet: {
       url: "https://http-testnet.hecochain.com",
