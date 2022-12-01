877d943a04e141e655550640742af1f7e5958911
yanjialun <119415571+yanjialun@users.noreply.github.com>
2022-12-01 12:07:39 +0800

    Update FT.sol

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -4,18 +4,42 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FT is ERC20 {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+    address _owner = msg.sender; 
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) { 
     }
 
     // TODO 实现mint的权限控制，只有owner可以mint
     function mint(address account, uint256 amount) external {
-
+        require(account != address(0),"mint to zero address");
+        require(account == msg.sender,"mint can't be created");
+        _mint(account, amount);
     }
 
     // TODO 用户只能燃烧自己的token
     function burn(uint256 amount) external {
-
+        _burn(msg.sender, amount);
+    }
+    bool isStopped = false;
+    modifier stopped {
+        require(!isStopped);
+        _;
+    }
+    modifier onlyAuthorized {
+        require(msg.sender == _owner);
+        _;
     }
 
-    // TODO 加分项：实现transfer可以暂停的逻辑
+    function stopTransFer() public onlyAuthorized {
+        isStopped = true;
+    }
+
+    function resumeTransFer() public onlyAuthorized {
+        isStopped = false;
+    }
+
+    function transfer(address to, uint256 amount) public virtual override stopped returns (bool){
+        address owner = msg.sender;
+        _transfer(owner, to, amount);
+        return true;
+    }
 }
