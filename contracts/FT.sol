732785d54e1e2947b35cfed73b595e43680423cc
wzhshuai <119606106+wzhshuai@users.noreply.github.com>
2022-12-01 22:48:19 +0800

    提交

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -4,16 +4,22 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FT is ERC20 {
+    address owner;
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        owner = msg.sender;
     }
 
     // TODO 实现mint的权限控制，只有owner可以mint
     function mint(address account, uint256 amount) external {
+        require(msg.sender == owner,"");
+        _mint(account, amount);
 
     }
 
-    // TODO 用户只能燃烧自己的token
+    // TODO
     function burn(uint256 amount) external {
+        require(allowance(owner, msg.sender)==amount,"");
+        _burn(msg.sender, amount);
 
     }
 
