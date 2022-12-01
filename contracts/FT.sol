abb211d888f30af260656ce5bda291a7c1f642f2
杨雯博 <2317523160@qq.com>
2022-12-01 22:29:26 +0800

    update

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -2,53 +2,38 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract FT is ERC20 {
-    
-    //管理员账户
-    address Administrator; 
-    bool _setTransfer;
-
+contract FT is ERC20, Ownable, Pausable {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
-        Administrator = msg.sender;
     }
 
     // TODO 实现mint的权限控制，只有owner可以mint
-    function mint(address account, uint256 amount) external {
-
-        require(msg.sender == Administrator);
+    function mint(address account, uint256 amount) external onlyOwner() {
         _mint(account, amount);
     }
 
     // TODO 用户只能燃烧自己的token
     function burn(uint256 amount) external {
-        require(balanceOf(msg.sender) >= amount);
         _burn(msg.sender, amount);
     }
-
-    modifier setTransfer() {
-        require(_setTransfer == true);
-        _;
-    }
-
     // TODO 加分项：实现transfer可以暂停的逻辑
-    function transfer(address to, uint256 amount)
-        public
-        override
-        setTransfer
-        returns (bool)
-    {
-        _transfer(msg.sender, to, amount);
-        return true;
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override {
+        require(!paused(), "contract is paused");
+        super._beforeTokenTransfer(from, to, amount);
     }
 
-    function setTransferFalse() external {
-        require(msg.sender == Administrator);
-        _setTransfer = false;
+    function setPause() public onlyOwner() {
+        _pause();
     }
 
-    function setTransferTrue() external {
-        require(msg.sender == Administrator);
-        _setTransfer = true;
+    function setUnPause() public onlyOwner() {
+        _unpause();
     }
+
 }
