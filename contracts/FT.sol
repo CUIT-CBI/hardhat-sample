c52618293bcb0da5e42c194c44f436cff0d6f11f
liuzhongwei <318757560@qq.com>
2022-12-01 22:02:47 +0800

    作业提交

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -4,18 +4,23 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FT is ERC20 {
+    address public _owner;
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _owner=msg.sender;
     }
 
     // TODO 实现mint的权限控制，只有owner可以mint
     function mint(address account, uint256 amount) external {
+        require(msg.sender=_owner,"you not mint");
+        _mint(account,amount);
 
     }
 
     // TODO 用户只能燃烧自己的token
     function burn(uint256 amount) external {
-
+        _burn(msg.sender,amount);
     }
 
     // TODO 加分项：实现transfer可以暂停的逻辑
+
 }
