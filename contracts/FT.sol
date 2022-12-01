bd53bb85fb5ebc756a1a4b8191011d04cc25e376
YangRuoyu2001 <119563122+YangRuoyu2001@users.noreply.github.com>
2022-12-01 16:03:50 +0800

    Update FT.sol

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -9,13 +9,50 @@ contract FT is ERC20 {
 
     // TODO 实现mint的权限控制，只有owner可以mint
     function mint(address account, uint256 amount) external {
+        require(account == _msgSender(), "Only owner can mint!");
+
+        _mint(account, amount);
 
     }
 
     // TODO 用户只能燃烧自己的token
+    
     function burn(uint256 amount) external {
-
+        address account = _msgSender();
+        _burn(account, amount);
     }
 
     // TODO 加分项：实现transfer可以暂停的逻辑
+    //这个不会，CSDN上搜的
+
+  event Pause();
+  event Unpause();
+ 
+  // 暂停状态，false表示未暂停，true表示暂停
+  bool public paused = false;
+ 
+  // 未暂停状态
+  modifier whenNotPaused() {
+    require(!paused);
+    _;
+  }
+ 
+  // 暂停状态
+  modifier whenPaused() {
+    require(paused);
+    _;
+  }
+ 
+  // 将合约状态改为暂停状态
+  function pause() whenNotPaused public {
+    paused = true;
+    emit Pause();
+  }
+ 
+  function unpause() whenPaused public {
+    paused = false;
+    emit Unpause();
+  }
+
 }
+
