bc4739ded8f98a004c5deba03598cd9117e90c62
Destiny-wei <1342348280@qq.com>
2022-12-01 22:08:55 +0800

    魏浩铭

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -5,24 +5,31 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NFT is ERC721 {
     uint256 internal totalSupplies;
+    uint256 public globalIndex;
+    // 全局tokenId下标
+    mapping(uint256 => uint256) public tokenIdToGlobalIndex;
     mapping(uint256 => uint256) public indexToTokenId;
-    mapping(address => uint256[]) public addressIndexToTokenId;
+    mapping(address => uint256[]) public ownerIndexToTokenId;
     // 此处为了与默认值0区分开来,故index从1开始,若为0说明未被mint
     mapping(uint256 => uint256) public tokenIdToIndex;
 
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {
         totalSupplies = 0;
+        globalIndex = 0;
     }
 
     function mint(address account, uint256 tokenId) external {
         super._mint(account, tokenId);
+        ownerIndexToTokenId[account].push(tokenId);
+        // index从1开始
+        tokenIdToIndex[tokenId] = ownerIndexToTokenId[account].length;
+        // 全局tokenId
+        indexToTokenId[globalIndex] = tokenId;
+        tokenIdToGlobalIndex[tokenId] = globalIndex;
         // 总供应量
         totalSupplies++;
-        addressIndexToTokenId[account].push(tokenId);
-        // index从1开始
-        tokenIdToIndex[tokenId] = addressIndexToTokenId[account].length;
-        // 全局tokenId
-        indexToTokenId[totalSupplies] = tokenId;
+        // 下标自增
+        globalIndex++;
     }
 
     function burn(uint256 tokenId) external {
@@ -36,7 +43,7 @@ contract NFT is ERC721 {
     }
 
     function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256) {
-        uint256[] memory tokens = addressIndexToTokenId[owner];
+        uint256[] memory tokens = ownerIndexToTokenId[owner];
         require(tokens.length > index, "index not have token");
         return tokens[index];
     }
@@ -46,7 +53,7 @@ contract NFT is ERC721 {
     }
 
     function removeList(uint256 tokenId) internal {
-        uint256[] storage tokens = addressIndexToTokenId[msg.sender];
+        uint256[] storage tokens = ownerIndexToTokenId[msg.sender];
         // 更新数组下标
         tokenIdToIndex[tokens[tokens.length - 1]] = tokenIdToIndex[tokenId];
         // 更新用户index下的tokenId数组
@@ -56,5 +63,9 @@ contract NFT is ERC721 {
         tokens.pop();
         // 删除tokenIdToIndex
         delete tokenIdToIndex[tokenId];
+        // 删除indexToTokenId
+        delete indexToTokenId[tokenIdToGlobalIndex[tokenId]];
+        // 删除tokenIdToGlobalIndex
+        delete tokenIdToGlobalIndex[tokenId];
     }
 }
