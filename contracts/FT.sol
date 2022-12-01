6502d1f7f5762bbee0f545cf8b7166f861871877
周哲 <1708834102@qq.com>
2022-12-01 22:29:25 +0800

    submit

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -2,20 +2,64 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
 
 contract FT is ERC20 {
+    // Set the owner address
+    address private owner;
+
+    // Set status of pause
+    bool private pause;
+
+    /**
+     * @dev Emitted when the pause is triggered by `account`.
+     */
+    event Paused(address account);
+
+    /**
+     * @dev Emitted when the pause is triggered by `account`.
+     */
+    event Unpaused(address account);
+    
+    /**
+     * @dev Initializes owner and pause.
+     */
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        owner = _msgSender();
+        pause = false;
     }
 
     // TODO 实现mint的权限控制，只有owner可以mint
     function mint(address account, uint256 amount) external {
-
+        require (_msgSender() == owner, "you are not the owner");
+        _mint(account, amount);
     }
 
     // TODO 用户只能燃烧自己的token
     function burn(uint256 amount) external {
+        _burn(_msgSender(), amount);
+    }
+    
+    // TODO 加分项：实现transfer可以暂停的逻辑
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        require (!pause, "token transfer while paused");
+        return super.transfer(to, amount);
+    }
+
+    /**
+     * @dev Triggers stopped state.
+     */
+    function _pause() external {
+        pause = true;
+        emit Paused(_msgSender());
 
     }
 
-    // TODO 加分项：实现transfer可以暂停的逻辑
+    /**
+     * @dev Returns to normal state.
+     */
+    function _unpause() internal {
+        pause = false;
+        emit Unpaused(_msgSender());
+    }
 }
--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -2,28 +2,42 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 contract NFT is ERC721 {
+    address private _owner;
+    uint256 private _totalSupply;
+    ERC721Enumerable enumerable;
+
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {
+        _owner = _msgSender();
     }
 
     function mint(address account, uint256 tokenId) external {
         // TODO
+        require (_msgSender() == _owner);
+        _mint(account, tokenId);
+        _totalSupply++;
     }
 
     function burn(uint256 tokenId) external {
         // TODO 用户只能燃烧自己的NFT
+        _burn(tokenId);
+        
     }
 
     function totalSupply() external view returns (uint256) {
         // TODO 获取总mint的NFT的数量
+        return _totalSupply;
     }
 
     function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256) {
         // TODO 加分项：根据用户的index，获取tokenId
+        return enumerable.tokenOfOwnerByIndex(owner, index);
     }
 
     function tokenByIndex(uint256 index) external view returns (uint256) {
         // TODO 根据index获取全局的tokenId
+        return enumerable.tokenByIndex(index);
     }
 }
