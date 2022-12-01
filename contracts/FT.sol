ecc905c5e53a9fbcefea5ffbc6bb27764999c38d
xiyang <90125263+JBossBC@users.noreply.github.com>
2022-12-01 21:34:33 +0800

    update ft and nft from xiyang

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -6,16 +6,28 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract FT is ERC20 {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
     }
-
+    modifier hasToken(address src){
+        //need judge _allowance???
+        require(balanceOf(src)>0);
+        _;
+    }
     // TODO 实现mint的权限控制，只有owner可以mint
-    function mint(address account, uint256 amount) external {
-
+    function mint(address account, uint256 amount) external  hasToken(account){
+              super._mint(account,amount);
     }
 
     // TODO 用户只能燃烧自己的token
     function burn(uint256 amount) external {
-
+       _burn(msg.sender, amount);
+    }
+     error pauseTransfer(string  explain );
+    // TODO 加分项：实现transfer可以暂停的逻辑
+       function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override {
+        revert pauseTransfer("parse transfer");
     }
 
-    // TODO 加分项：实现transfer可以暂停的逻辑
 }
--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -2,21 +2,50 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
+contract Link{
+    tokenNode private header;
+    struct tokenNode {
+        uint256 val;
+        tokenNode nextNode;
+    }
+    function getLinkHeader()public pure returns(tokenNode){
+        return header;
+    }
+    function addNode(tokenNode memory node)public{
+        tokenNode memory temp=header;
+           while(temp.next)
+    } 
+}
 contract NFT is ERC721 {
+    uint256 private totalSupplyNumber;
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {
     }
 
     function mint(address account, uint256 tokenId) external {
         // TODO
+        // tokenId need be not created by anyone
+        _mint(account,tokenId);
+    }
+     function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256, /* firstTokenId */
+        uint256 batchSize
+    ) internal override {
+        super._beforeTokenTransfer(from,to,0,batchSize);
+        if(from==address(0)){
+            totalSupplyNumber=totalSupplyNumber++;
+        }
     }
 
     function burn(uint256 tokenId) external {
         // TODO 用户只能燃烧自己的NFT
+        _burn(tokenId);
     }
 
     function totalSupply() external view returns (uint256) {
         // TODO 获取总mint的NFT的数量
+        return totalSupplyNumber;
     }
 
     function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256) {
