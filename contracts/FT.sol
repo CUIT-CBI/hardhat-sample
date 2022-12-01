bdaa25b65ad3c2ef6c38ad3f90366803ba655b50
ZedViper <2176914057@qq.com>
2022-12-01 21:46:15 +0800

    2020131071-fw

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -2,20 +2,29 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 
-contract FT is ERC20 {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
-    }
+contract FT is ERC20 ,Pausable{
+
+    address  owner = msg.sender;
+    constructor(string memory name, string memory symbol) 
+        ERC20(name, symbol) {}
 
     // TODO 实现mint的权限控制，只有owner可以mint
     function mint(address account, uint256 amount) external {
+        require(owner == msg.sender);
+        _mint(account, amount);
 
     }
 
     // TODO 用户只能燃烧自己的token
     function burn(uint256 amount) external {
-
+        _burn(msg.sender,amount);
     }
 
     // TODO 加分项：实现transfer可以暂停的逻辑
+  function __transfer(address account, uint amount) public whenNotPaused {
+        _transfer(msg.sender,account,amount);
+   }
+
 }
