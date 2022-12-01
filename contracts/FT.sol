0b71ca9400ae0c2c46475cd25f9f8899c7bd5eef
王泽源 <1936587036@qq.com>
2022-12-01 11:43:44 +0800

    王泽源的第二次

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -4,18 +4,39 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FT is ERC20 {
+    address owner;
+    bool open=false;
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        owner=msg.sender;
     }
 
     // TODO 实现mint的权限控制，只有owner可以mint
     function mint(address account, uint256 amount) external {
-
+        require(account==owner, "Only owner can mint");
+        _mint(account, amount);
     }
 
     // TODO 用户只能燃烧自己的token
     function burn(uint256 amount) external {
-
+        require(amount<= msg.sender.balance,"your balance is not enough");
+        _burn(msg.sender, amount);
     }
 
     // TODO 加分项：实现transfer可以暂停的逻辑
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        require(open, "sorry,the transfer is closed");
+        address owner = msg.sender;
+        _transfer(owner, to, amount);
+        return true;
+    }
+    function lock()external returns(bool){
+        require(msg.sender == owner, "you are not the owner");
+        open=false;
+        return open;
+    }
+    function unlock()external returns(bool){
+        require(msg.sender == owner, "you are not the owner");
+        open=true;
+        return open;
+    }
 }
--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -4,26 +4,53 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NFT is ERC721 {
+    uint sum=0;
+    mapping (uint =>uint) public totalId;
+    mapping (address =>mapping(uint => uint))public ownerIndex;
+    mapping(address=>uint)public number;
+    mapping(uint=>uint)public idToIndex;
+    mapping (address =>mapping(uint => uint))public indexOwner;
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {
     }
-
+    
     function mint(address account, uint256 tokenId) external {
         // TODO
+        _safeMint(account, tokenId);
+        totalId[sum]=tokenId;
+        idToIndex[tokenId]=sum;
+        sum=sum+1;
+        ownerIndex[account][number[account]]=tokenId;
+        indexOwner[account][tokenId]=number[account];
+        number[account] +=1;
     }
 
-    function burn(uint256 tokenId) external {
+    function burn(uint256 tokenId) external{
         // TODO 用户只能燃烧自己的NFT
+        require(msg.sender == ownerOf(tokenId),"you are not the owner of this NFT");
+        _burn(tokenId);
+        sum=sum-1;
+        totalId[idToIndex[tokenId]]=totalId[sum];
+        totalId[sum]=0;
+        number[msg.sender]=number[msg.sender]-1;
+        ownerIndex[msg.sender][indexOwner[msg.sender][tokenId]]=ownerIndex[msg.sender][number[msg.sender]];
+        ownerIndex[msg.sender][number[msg.sender]]=0;
+        
     }
 
     function totalSupply() external view returns (uint256) {
         // TODO 获取总mint的NFT的数量
+        return sum;
     }
 
     function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256) {
         // TODO 加分项：根据用户的index，获取tokenId
+        require(number[owner] >index,"Error:index overflow");
+        return ownerIndex[owner][index];
     }
 
     function tokenByIndex(uint256 index) external view returns (uint256) {
         // TODO 根据index获取全局的tokenId
+        require(sum > index,"Error:index overflow");
+        return totalId[index];
     }
 }
