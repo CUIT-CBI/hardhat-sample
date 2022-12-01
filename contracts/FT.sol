af5df972284ce0d0c37f50f1a0871566bef6d90b
quyukai <119607636+quyukai@users.noreply.github.com>
2022-12-01 23:27:12 +0800

    Update FT.sol
    2020131066

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -2,20 +2,29 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 
 contract FT is ERC20 {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        owner = msg.sender;
     }
 
     // TODO 实现mint的权限控制，只有owner可以mint
     function mint(address account, uint256 amount) external {
+        require(msg.sender == owner);
+        _mint(account,amount);
 
     }
 
     // TODO 用户只能燃烧自己的token
     function burn(uint256 amount) external {
+         require(msg.sender == owner);
+        _burn(msg.sender,amount);
 
     }
 
     // TODO 加分项：实现transfer可以暂停的逻辑
+    function transferStop(address account,uint256 amount) public whenNotPaused{
+        _transfer(msg.sender,account,amount);
+    }
 }
