969dd30d2b00de38b75252712897636be0ee564d
GSY <2846141630@qq.com>
2022-12-01 20:53:17 +0800

    2020131073-gsy

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,23 +7,58 @@ contract NFT is ERC721 {
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {
     }
 
+    mapping(uint256 => address) private _owners;
+    mapping(address => uint256) public ownerToIndex;
+    mapping(uint256 => uint256[]) public indexToTokenid;
+    uint256[] tokenArr;
+    uint256 _index;
+    uint256 tokenNum;
+
     function mint(address account, uint256 tokenId) external {
         // TODO
+        _owners[tokenId] = account;
+        //判断账户是否在映射里
+        for (uint256 i=0;i<=tokenId;i++) {
+            if (account == _owners[i]) {
+                break;
+            }else{
+                _index +=1;
+                ownerToIndex[account] = _index;
+            }
+        }
+        //将tokenId加入数组tokenArr
+        tokenArr.push(tokenId);
+        indexToTokenid[_index] = tokenArr;
+        _mint(account,tokenId);
+        tokenNum += 1;
     }
 
     function burn(uint256 tokenId) external {
         // TODO 用户只能燃烧自己的NFT
+        require(msg.sender == _owners[tokenId]);
+        _burn(tokenId);
+        delete tokenArr[tokenId-1]; 
+        delete _owners[tokenId];
+        tokenNum -=1;
     }
 
     function totalSupply() external view returns (uint256) {
         // TODO 获取总mint的NFT的数量
+        return tokenNum; 
     }
 
     function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256) {
         // TODO 加分项：根据用户的index，获取tokenId
+        //require(ownerToIndex[owner] == index);
+        //uint256[] memory arr = indexToTokenid[index];
+        //for(uint256 i=0;i<=arr.length;i++){
+          //  return arr[i];
+        //}
+    
     }
 
     function tokenByIndex(uint256 index) external view returns (uint256) {
         // TODO 根据index获取全局的tokenId
+
     }
 }
