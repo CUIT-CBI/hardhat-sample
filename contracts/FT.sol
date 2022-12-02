7831cb025a3e568a2fe996cd19447c7716d75a9b
xiaomigiao <111137477+xiaomigiao@users.noreply.github.com>
2022-12-02 20:10:39 +0800

    Update FT.sol

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -2,20 +2,36 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import "./Lock.sol";
 
-contract FT is ERC20 {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+contract FT is ERC20{
+    address private Owner;
+    
+
+    constructor(string memory name, string memory symbol,uint256 _unlockTime) ERC20(name, symbol) 
+    // Lock(_unlockTime) payable
+    {
+        Owner = msg.sender;
+    }
+
+    modifier onlyOwner(){
+        require(Owner == msg.sender);
+        _;
     }
 
     // TODO 实现mint的权限控制，只有owner可以mint
-    function mint(address account, uint256 amount) external {
-
+    function mint(address account, uint256 amount) external onlyOwner{
+        require(amount>0);
+        _mint(account,amount);
     }
 
     // TODO 用户只能燃烧自己的token
     function burn(uint256 amount) external {
-
+        _burn(msg.sender,amount);
     }
 
     // TODO 加分项：实现transfer可以暂停的逻辑
+    function ContinueTransfer() external {
+        // withdraw();
+    }
 }
