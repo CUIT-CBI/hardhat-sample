ae7532ba6b50d9e81d5d6302d64e2965717985c2
KE456839 <694114245@qq.com>
2022-12-02 10:02:10 +0800

    2020131130

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NFT is ERC721 {
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {
