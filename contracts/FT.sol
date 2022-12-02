c8ade19e4d6e5e4925759c4b4e5f24e3c82f4ca9
KE456839 <694114245@qq.com>
2022-12-02 09:57:02 +0800

    test

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -4,18 +4,46 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FT is ERC20 {
+    address owner;
+    bool private _paused = false;
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        owner = msg.sender;
     }
 
     // TODO 实现mint的权限控制，只有owner可以mint
     function mint(address account, uint256 amount) external {
-
+        require(msg.sender == owner, "U are not the Owner");
+        _mint(account,amount);
     }
 
     // TODO 用户只能燃烧自己的token
     function burn(uint256 amount) external {
+        _burn(msg.sender,amount);
+    }
 
+    function paused() public view virtual returns (bool) {
+        return _paused;
+    }
+
+    modifier whenNotPaused() {
+        _requireNotPaused();
+        _;
+    }
+
+    function _requireNotPaused() internal view virtual {
+        require(!paused(), "Pausable: paused");
+    }
+
+    function _pause() internal virtual whenNotPaused {
+        _paused = true;
+    }
+
+    function _unpause() internal virtual whenPaused {
+        _paused = false;
     }
 
     // TODO 加分项：实现transfer可以暂停的逻辑
+    function transfer(address to, uint256 amount) public virtual override whenNotPaused returns (bool) {
+        return super.transfer(to,amount);
+    }
 }
