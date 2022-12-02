751b50b9f55c22db6634b046f3d42897ac8ec6f6
dongnuo <1827385080@qq.com>
2022-12-02 22:29:06 +0800

    2020131019

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contractscurity/Pausable.sol";
+import "@openzeppelin/contracts/curity/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
