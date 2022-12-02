411fc53cca4616768e538024b83b92d0c4cfef1f
wenjunkai <1851931181@qq.com>
2022-12-02 22:42:36 +0800

    21

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -2,20 +2,38 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contractscurity/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract FT is ERC20 {
+
+contract FT is ERC20 ,Pausable, Ownable{
+
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+
     }
-
-    // TODO 实现mint的权限控制，只有owner可以mint
-    function mint(address account, uint256 amount) external {
-
+    function mint(address account, uint256 amount) onlyOwner external {
+        _mint(account,amount);
     }
 
     // TODO 用户只能燃烧自己的token
     function burn(uint256 amount) external {
-
+        _burn(msg.sender,amount);
     }
 
     // TODO 加分项：实现transfer可以暂停的逻辑
+   function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
 }
