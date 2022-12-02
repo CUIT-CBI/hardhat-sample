161e417ea37a2ed5c319cf1b21691a3b89bc1ec7
Kevin <qingchen0919@qq.com>
2022-12-02 22:42:44 +0800

    Update NT.sol

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -7,14 +7,24 @@ contract FT is ERC20 {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
     }
 
+    mapping(address => uint256) private _balances;
+    uint256 private _totalSupply;
+
     // TODO 实现mint的权限控制，只有owner可以mint
     function mint(address account, uint256 amount) external {
-
+        require(msg.sender == account);
+        require(account !=address(0),'ERC20: mint to the zero address!');
+       _totalSupply = _totalSupply+amount;
+        _balances[account] = _balances[account]+amount;
+  
     }
 
     // TODO 用户只能燃烧自己的token
     function burn(uint256 amount) external {
+        require(msg.sender != address(0), 'ERC20: burn from the zero address');
 
+        _balances[msg.sender] = _balances[msg.sender]-amount;
+       _totalSupply= _totalSupply-amount;
     }
 
     // TODO 加分项：实现transfer可以暂停的逻辑
