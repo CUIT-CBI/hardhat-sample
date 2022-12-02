e9e7d40e5356b808ade9b9b43c79e9f1fd566c9d
STAR-FALL-00 <77623276+STAR-FALL-00@users.noreply.github.com>
2022-12-02 14:14:54 +0800

    Create FT.sol

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -4,17 +4,21 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FT is ERC20 {
+    address owner;
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+         owner = msg.sender;
     }
 
     // TODO 实现mint的权限控制，只有owner可以mint
     function mint(address account, uint256 amount) external {
+        require(msg.sender == owner);
+        _mint(account,amount);
 
     }
 
     // TODO 用户只能燃烧自己的token
     function burn(uint256 amount) external {
-
+        _burn(msg.sender,amount);
     }
 
     // TODO 加分项：实现transfer可以暂停的逻辑
