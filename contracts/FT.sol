4da1d2b39e9c1935349366e950c037e880fd8f80
ekatsi <119686933+ekatsi@users.noreply.github.com>
2022-12-02 23:35:08 +0800

    Create FT.sol

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -1,39 +1,29 @@
-// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contractscurity/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
 
-
-contract CYYFT is ERC20, Ownable, Pausible{
+contract FT is ERC20,Pausable,Ownable {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
     }
 
-    //TODO 实现mint的权限控制
-    function mint(address account, uint256 amount) external onlyOwner() {
+    // TODO 实现mint的权限控制，只有owner可以mint
+    function mint(address account, uint256 amount) external onlyOwner {
         _mint(account, amount);
     }
 
-    //TODO shixian用户只能燃烧自己的token
+    // TODO 用户只能燃烧自己的token
     function burn(uint256 amount) external {
-        _burn(msg.sender, amount);
+        _burn(msg.sender,amount);
     }
 
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal override {
-        require(!paused(), "contract is paused");
-        super._beforeTokenTransfer(from, to, amount);
+    // TODO 加分项：实现transfer可以暂停的逻辑
+    function transfer(address to, uint256 amount) public virtual override whenNotPaused returns (bool) {
+        return super.transfer(to,amount);
     }
 
-    function setPause() public onlyOwner() {
+    function GetPause() public onlyOwner{
         _pause();
-    }
-
-    function setUnPause() public onlyOwner() {
-        _unpause();
     }
 }
