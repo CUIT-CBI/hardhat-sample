5216bd8f131687dcdbe402a9c44ddc5530cdc0c1
zhidonghan <1154599039@qq.com>
2022-12-07 12:07:38 +0800

    2020131022-何思辰

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -2,36 +2,38 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contractscurity/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract FT is ERC20,Pausable{
+
+contract FT is ERC20 ,Pausable, Ownable{
+
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
-        owner = msg.sender;
+
     }
-    
-    modifier OnlyOwner{
-        require (msg.sender == owner,"you don't have permission");
-        _;
-    }
-    
-    function mint(address account, uint256 amount) external OnlyOwner{
-        super._mint(account, amount);
+    function mint(address account, uint256 amount) onlyOwner external {
+        _mint(account,amount);
     }
 
+    // TODO 用户只能燃烧自己的token
     function burn(uint256 amount) external {
-        super._burn(msg.sender,amount);
+        _burn(msg.sender,amount);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override {
-        require(!paused(), "contract is paused");
-        super._beforeTokenTransfer(from, to, amount);
-    }
-    
-    function Pause() external onlyOwner{
+    // TODO 加分项：实现transfer可以暂停的逻辑
+   function pause() public onlyOwner {
         _pause();
     }
-    
-    function Unpause() external onlyOwner{
-        _burn(msg.sender, amount);
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
     }
 }
