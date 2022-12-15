cf5d96dcf25f9116165a2ae80f8f71163a3a05a7
BernardLowe <vitalikzhou@gmail.com>
2022-12-15 21:14:29 +0800

    FT test

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -3,35 +3,29 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract FT is ERC20,Pausable{
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
-        owner = msg.sender;
-    }
+contract FT is ERC20, Pausable, Ownable {
+
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
     
-    modifier OnlyOwner{
-        require (msg.sender == owner,"you don't have permission");
-        _;
-    }
-    
-    function mint(address account, uint256 amount) external OnlyOwner{
-        super._mint(account, amount);
+    function mint(address account, uint256 amount) external onlyOwner {
+        _mint(account, amount);
     }
 
     function burn(uint256 amount) external {
-        super._burn(msg.sender,amount);
+        _burn(msg.sender,amount);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override {
-        require(!paused(), "contract is paused");
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override whenNotPaused {
         super._beforeTokenTransfer(from, to, amount);
     }
     
-    function Pause() external onlyOwner{
+    function pause() external onlyOwner {
         _pause();
     }
     
-    function Unpause() external onlyOwner{
-        _burn(msg.sender, amount);
+    function unpause() external onlyOwner {
+        _unpause();
     }
 }
---
+++ test/FT.ts
@@ -0,0 +1,58 @@
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { BigNumber } from "ethers";
+
+describe("FT", function () {
+  const symbol = 'TFT'
+  const name = 'TestFT'
+  let ft
+  // We define a fixture to reuse the same setup in every test.
+  // We use loadFixture to run this setup once, snapshot that state,
+  // and reset Hardhat Network to that snapshot in every test.
+  async function deployFT() {
+    // Contracts are deployed using the first signer/account by default
+    const FT = await ethers.getContractFactory("FT");
+    ft = await FT.deploy(name, symbol);
+
+    return { ft };
+  }
+
+  describe("Deployment", function () {
+    it("Should have right symbol", async function () {
+      const { ft } = await loadFixture(deployFT);
+
+      expect(await ft.symbol()).to.equal(symbol);
+    });
+
+    it("Should have right name", async function () {
+      const { ft } = await loadFixture(deployFT);
+
+      expect(await ft.name()).to.equal(name);
+    });
+  });
+
+  describe("Mintable", function () {
+    it("Should mintable by owner", async function () {
+      const { ft } = await loadFixture(deployFT);
+      const [owner] = await ethers.getSigners();
+      const signedFt = ft.connect(owner)
+      const amount = 100
+      await signedFt.mint(owner.address, amount)
+
+      expect(await ft.balanceOf(owner.address)).to.equal(amount);
+    });
+
+    it("Should not mintable by other account", async function () {
+      const { ft } = await loadFixture(deployFT);
+      const [owner, otherAccount] = await ethers.getSigners();
+      const signedFt = ft.connect(otherAccount)
+      const amount = 100
+
+      signedFt.mint(otherAccount.address, amount).catch(async (e) => {
+        expect(await ft.balanceOf(owner.address)).to.equal(0);
+      })
+    });
+  });
+});
