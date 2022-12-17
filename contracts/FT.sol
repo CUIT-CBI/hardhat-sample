577a926a94f49b4114a90e51367ae498415ce4f4
X1aZhongwen <114158080+X1aZhongwen@users.noreply.github.com>
2022-12-17 19:16:24 +0800

    Update FT.sol

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -2,30 +2,38 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
 
-contract FT is ERC20 ,Pausable{
-
-    address  owner = msg.sender;
-    constructor(string memory name, string memory symbol) 
-        ERC20(name, symbol) {}
+contract FT is ERC20 {
+    address Creator;
+    bool TransferState;
+    constructor(string memory name, string memory symbol) ERC20(name,symbol) {
+        Creator = msg.sender;
+    }
 
     // TODO 实现mint的权限控制，只有owner可以mint
     function mint(address account, uint256 amount) external {
-        require(owner == msg.sender,'you are not the owner!');
-        _mint(account, amount);
-
+        require(msg.sender == Creator);
+        _mint(account,amount);
     }
 
     // TODO 用户只能燃烧自己的token
     function burn(uint256 amount) external {
-        require(owner == msg.sender,'you are not the owner!');
         _burn(msg.sender,amount);
     }
-
+    
     // TODO 加分项：实现transfer可以暂停的逻辑
-  function __transfer(address account, uint amount) public whenNotPaused {
-        _transfer(msg.sender,account,amount);
-   }
-
+    function transfer(address sender,address recipient,uint256 amount) external returns(bool){
+        require(TransferState == true);
+        _transfer(sender, recipient, amount);
+        return true;
+    }
+    function transferState(bool _state) external returns(bool){
+        require(msg.sender == Creator);
+        if(_state == true){
+            TransferState = true;
+        }else{
+            TransferState = false;
+        }
+        return true;
+    }
 }
