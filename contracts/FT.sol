26f69503f792fa9e0254195a2826f168579ead8e
李雯雯 <1142382021@qq.com>
2022-12-18 19:51:46 +0800

    lww

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -4,18 +4,44 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FT is ERC20 {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
-    }
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
 
     // TODO 实现mint的权限控制，只有owner可以mint
     function mint(address account, uint256 amount) external {
-
+        require(account != address(0), "ERC20: mint to the zero address");
+        require(account == msg.sender, "only owner can mint");
+        require(amount > 0, "amount");
+        super._mint(account, amount);
     }
 
     // TODO 用户只能燃烧自己的token
     function burn(uint256 amount) external {
-
+        require(amount > 0);
+        super._mint(msg.sender, amount);
     }
 
     // TODO 加分项：实现transfer可以暂停的逻辑
+    event Pause();
+    event Unpause();
+    bool public paused = false;
+
+    modifier whenNotPaused() {
+        require(!paused);
+        _;
+    }
+
+    modifier whenPaused() {
+        require(paused);
+        _;
+    }
+
+    function pause() public whenNotPaused {
+        paused = true;
+        emit Pause();
+    }
+
+    function unpause() public whenPaused {
+        paused = false;
+        emit Unpause();
+    }
 }
