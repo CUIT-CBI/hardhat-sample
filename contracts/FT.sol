5580c01a465ac8bfabc337c976eeb7804e2339a0
t-yundong <1250432405@qq.com>
2023-02-19 17:30:24 +0800

    2020131158

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -1,30 +1,50 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
-
+@@ -4,18 +4,48 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract FT is ERC20, Pausable, Ownable {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
-    
-    function mint(address account, uint256 amount) external onlyOwner {
+contract FT is ERC20 {
+    address Administrator; //管理员账户
+    bool _setTransfer;
+
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        Administrator = msg.sender;
+    }
+
+    // TODO 实现mint的权限控制，只有owner可以mint
+    function mint(address account, uint256 amount) external {
+
+        require(msg.sender == Administrator);
         _mint(account, amount);
     }
 
+    // TODO 用户只能燃烧自己的token
     function burn(uint256 amount) external {
+        require(balanceOf(msg.sender) >= amount);
         _burn(msg.sender, amount);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override whenNotPaused {
-        super._beforeTokenTransfer(from, to, amount);
+    modifier setTransfer() {
+        require(_setTransfer == true);
+        _;
     }
-    
-    function pause() external onlyOwner {
-        _pause();
+
+    // TODO 加分项：实现transfer可以暂停的逻辑
+    function transfer(address to, uint256 amount)
+        public
+        override
+        setTransfer
+        returns (bool)
+    {
+        _transfer(msg.sender, to, amount);
+        return true;
     }
-    
-    function unpause() external onlyOwner {
-        _unpause();
+
+    function setTransferFalse() external {
+        require(msg.sender == Administrator);
+        _setTransfer = false;
+    }
+
+    function setTransferTrue() external {
+        require(msg.sender == Administrator);
+        _setTransfer = true;
     }
 }
